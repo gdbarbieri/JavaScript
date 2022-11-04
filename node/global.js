3ddc4a4b6d52c187125eb87403e677d8a81c9f2e
Guilherme Barbieri <gdbarbieri33@gmail.com>
2022-11-04 16:11:38 -0300

    JS

---
+++ node/global.js
@@ -0,0 +1,7 @@
+//console.log(global)
+global.MinhaApp = Object.freeze( {
+    saudacao(){
+        return 'Hello World'
+    },
+    nome: 'Soraya'
+})
---
+++ node/passandoParametrosClientes.js
@@ -0,0 +1,2 @@
+const saudacoes = require('./passandoParametros')('Ana','Lucas','Gui')
+console.log(saudacoes)
