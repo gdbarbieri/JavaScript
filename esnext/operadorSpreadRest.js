8e99124cfd4d4d609e472e6146ef92fecfedc8e5
Guilherme Barbieri <gdbarbieri33@gmail.com>
2022-11-10 16:26:11 -0300

    JS

--- esnext/operadorSpreadRest.js
+++ esnext/operadorSpreadRest.js
@@ -1,2 +1,12 @@
 // operador ... rest(juntar)/spread(espalhar)
 //usar rest com parâmetro de função 
+
+//usar spread com objeto 
+const funcionario = {nome: 'Maria', salario: 1234.99}
+const clone = {ativo: true, ...funcionario}
+console.log(clone)
+
+// usar spread com array 
+const grupoA = ['Guilherme','Mariana', 'Marina']
+const grupoFinal =  ['Breno',  ...grupoA, 'Naves']
+console.log(grupoFinal)
